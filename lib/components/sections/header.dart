da56f3a734dd1059052a1c525bc1848e050a68ee
eri-akio <erikmatsumoto18@gmail.com>
2024-07-02 07:37:56 -0300

    Implementando temas

--- lib/components/sections/header.dart
+++ lib/components/sections/header.dart
@@ -1,47 +1,40 @@
+import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatelessWidget {
-  const Header({super.key});
+  const Header({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: <Color>[
-            Color.fromRGBO(103, 99, 234, 1.0),
-            Color.fromRGBO(155, 105, 254, 1.0),
-            Color.fromRGBO(195, 107, 255, 1.0),
-          ]
-        ),
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: ThemeColors.headerGradient),
         borderRadius: BorderRadius.vertical(bottom: Radius.circular(10)),
       ),
-      child: const Padding(
-        padding: EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
+                // Text('\$1000.00', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),),
                 Text.rich(
                   TextSpan(
-                      text: '\$',
-                      style: TextStyle(fontSize: 16),
-                      children: <TextSpan>[
-                        TextSpan(
-                          text: '1000.00',
-                          style: TextStyle(
-                              fontSize: 28, fontWeight: FontWeight.bold),
-                        ),
-                      ]),
+                    text: '\$',
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
                 ),
-                Text(
-                  'Balanço disponível',
-                  style: TextStyle(fontSize: 16),
-                ),
+                Text('Balanço disponível'),
               ],
             ),
             Icon(
